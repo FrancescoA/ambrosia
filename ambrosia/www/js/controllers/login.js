8d336304daab663521c8102982955bf4f716d90e
Franesco Agosti <fra5@duke.edu>
2014-11-15 18:08:28 -0500

    got facebook login working

--- ambrosia/www/js/controllers/login.js
+++ ambrosia/www/js/controllers/login.js
@@ -1,8 +1,16 @@
 ambrosia.controller('LoginCtrl', function($scope, FIREBASE_REF, $firebaseSimpleLogin,userSession) {
 	userSession.auth=$firebaseSimpleLogin(new Firebase(FIREBASE_REF));
+	
+	console.log(FIREBASE_REF);
+
+	var firebase = new Firebase(FIREBASE_REF);
+
 
     $scope.login=function(provider){
-        userSession.auth.$login(provider);
+    	console.log(provider);
+        firebase.authWithOAuthPopup(provider, function(error, authData) {
+        	console.log(authData);
+        });
     }
 
 })
