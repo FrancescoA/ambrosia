f26d5d0249e6da4fccdc26ae6b5dd04c6ac442ca
Franesco Agosti <fra5@duke.edu>
2014-11-16 13:34:42 -0500

    pretty much done

--- ambrosia/www/js/controllers/searchResult.js
+++ ambrosia/www/js/controllers/searchResult.js
@@ -1,9 +1,21 @@
-ambrosia.controller('SearchResultCtrl', function($scope, foodService) {
+ambrosia.controller('SearchResultCtrl', function($scope, foodService, $ionicPopup, userService) {
 
 
   $scope.foods = foodService.queryResult;
 
   $scope.show_section = {};
+
+
+  $scope.showConfirm = function(item) {
+   var confirmPopup = $ionicPopup.confirm({
+     title: 'Add to planner',
+     template: 'Are you sure you want add this to your planner?'
+   });
+   confirmPopup.then(function(res) {
+      userService.addToPlanner(userSession.user.uid, item);
+   });
+ }
+
 
 	$scope.section_click = function(section, $event) {
       console.log(section);
