725ef79e3e7bc0b94567433b4bbd72fbd25c7e34
Pengyi Pan <nickpanmail@gmail.com>
2014-11-16 04:28:37 -0500

    merge

--- ambrosia/www/js/controllers/favorites.js
+++ ambrosia/www/js/controllers/favorites.js
@@ -1,3 +1,11 @@
 ambrosia.controller('FavoritesCtrl', function($scope) {
 	console.log("FavoritesCtrl");
+
+	$scope.heartOutlineImg = "icon ion-ios7-heart-outline"
+	$scope.heartImg = "icon ion-ios7-heart"
+
+	$scope.currentImg = "icon ion-ios7-heart-outline"
+
+
+
 });
