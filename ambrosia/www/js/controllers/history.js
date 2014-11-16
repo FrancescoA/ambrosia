73294b2d8211c916c24c7e66b305e4ef078f1dfa
Franesco Agosti <fra5@duke.edu>
2014-11-16 12:03:33 -0500

    here sherry

--- ambrosia/www/js/controllers/history.js
+++ ambrosia/www/js/controllers/history.js
@@ -5,5 +5,23 @@ ambrosia.controller('HistoryCtrl', function($scope, userService) {
 
 
 	$scope.isAnalyze = false;
-	
+
+
+	var historySum = function(history) {
+		var protein = {name: 'protein', value: 0};
+		var carbs = {name: 'carbs', value: 0};
+		var fat = {name: 'fat', value: 0};
+
+
+		for(var i = 0 ; i < history.length; i++){
+			var item = history[i];
+			protein.value += item.nf_protein;
+			carbs.value += item.nf_total_carbohydrate;
+			fat.value += item.nf_total_fat;
+		}
+		return [protein,carbs,fat]
+	}
+
+	console.log(historySum($scope.history));
+
 })
