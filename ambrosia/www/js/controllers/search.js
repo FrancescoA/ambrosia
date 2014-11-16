e672f73e507e12d3668abdd7224a1e2bd5fa167e
Pengyi Pan <nickpanmail@gmail.com>
2014-11-16 02:23:20 -0500

    finished search view

--- ambrosia/www/js/controllers/search.js
+++ ambrosia/www/js/controllers/search.js
@@ -1,13 +1,43 @@
 ambrosia.controller('SearchCtrl', function($scope) {
 
+	$scope.p_lowerLimit = 0;
+	$scope.p_upperLimit = 0;
+    $scope.c_lowerLimit = 0;
+    $scope.c_upperLimit = 0;
+    $scope.f_lowerLimit = 0;
+    $scope.f_upperLimit = 0;
+
+    $scope.proteinToCal = 4;
+    $scope.carbToCal = 4;
+    $scope.fatToCal = 9;
+    $scope.total_lowerLimit = 0;
+    $scope.total_upperLimit = 0;
+
   $('.nstSlider').nstSlider({
     "left_grip_selector": ".leftGrip",
     "right_grip_selector": ".rightGrip",
     "value_bar_selector": ".bar",
     "value_changed_callback": function(cause, leftValue, rightValue) {
-        $(this).parent().find('.leftLabel').text(leftValue);
-        $(this).parent().find('.rightLabel').text(rightValue);
+        $(this).parent().find('.leftLabel').text(leftValue); //not using
+        $(this).parent().find('.rightLabel').text(rightValue); //not using
         $(this).parent().find('.item-note').text(leftValue + "  -  " + rightValue + " (g)");
+        if ($(this).attr("name") == "protein" ){
+        	$scope.p_lowerLimit = leftValue;
+            $scope.p_upperLimit = rightValue;
+        }
+        else if ($(this).attr("name") == "carb" ){
+            $scope.c_lowerLimit = leftValue;
+            $scope.c_upperLimit = rightValue;            
+        }
+        else if ($(this).attr("name") == "fat" ){
+            $scope.f_lowerLimit = leftValue;
+            $scope.f_upperLimit = rightValue;            
+        }
+        $scope.total_lowerLimit = $scope.p_lowerLimit * $scope.proteinToCal + $scope.c_lowerLimit * $scope.carbToCal + $scope.f_lowerLimit * $scope.fatToCal;
+        $scope.total_upperLimit = $scope.p_upperLimit * $scope.proteinToCal + $scope.c_upperLimit * $scope.carbToCal + $scope.f_upperLimit * $scope.fatToCal;
+        $('[name="totalCal"]').find('.item-note').text($scope.total_lowerLimit + "  -  " + $scope.total_upperLimit);
+        
+        
     }
 });
 
